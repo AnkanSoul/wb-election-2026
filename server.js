23fd51d93e04f83d916639f0ee7030e41e7a65f8
Ankan Chakraborty <ankandgp37@gmail.com>
2026-05-04 17:03:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,3 +37,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🔥 API running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
